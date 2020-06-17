37f15f74d15fc9139a574632f5009eb41820cd63
AbdelRahman Tarek <abdelrahman.tare.mahdy@gmail.com>
2020-06-17 20:30:13 +0200

    login form added

--- frontend/src/components/NewsCard.vue
+++ frontend/src/components/NewsCard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card dense>
-    <v-card-title>{{article.title}}</v-card-title>
+    <v-card-title class="text-end">{{article.title}}</v-card-title>
     <v-card-subtitle class="text-right">{{article.author}}</v-card-subtitle>
     <v-card-text class="text-right">{{article.description}}</v-card-text>
     <v-img :src="article.urlToImage" height="300px"></v-img>
--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,15 +1,50 @@
 <template>
-  <div>
-      Register
-  </div>
+  <v-container class="justify-center" fluid px-12 mb-3 mt-3>
+    <v-form ref="form" v-model="valid">
+      <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+      <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+      <v-date-picker
+        v-model="date"
+        full-width
+        :landscape="$vuetify.breakpoint.smAndUp"
+        class="mt-4"
+      ></v-date-picker>
+      <v-card-actions class="justify-end">
+        <v-btn color="error" class="mr-4" @click="reset">Reset</v-btn>
+        <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Register</v-btn>
+      </v-card-actions>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: 'Register'
-}
+  name: "Register",
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      v => !!v || "Name is required",
+      v => (v && v.length <= 10) || "Name must be less than 10 characters"
+    ],
+    email: "",
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ],
+    date: new Date().toISOString().substr(0, 10)
+  }),
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+      console.log('name', this.name, 'email', this.email, "date", this.date);
+    },
+    reset() {
+      this.$refs.form.reset();
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
